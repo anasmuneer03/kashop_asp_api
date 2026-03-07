374c505ebceaeb4c9031aa27aeac31d6d3c9074a
anasmuneer03 <anasqasem656@gmail.com>
2026-03-07 14:21:22 +0300

    project Architecture, connection with DB

---
+++ KASHOP.PL/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using KASHOP.DAL.Data;
+using KASHOP.PL.Resources;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace KASHOP.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IStringLocalizer _localizer;
+        public CategoriesController(ApplicationDbContext context, IStringLocalizer<SharedResources> localizer)
+        {
+            _context = context;
+            _localizer = localizer;
+        }
+        [HttpGet("")]
+        public IActionResult GetAll()
+        {
+            return Ok(new 
+            { 
+                _localizer["Success"].Value 
+            });
+        }
+    }
+}
